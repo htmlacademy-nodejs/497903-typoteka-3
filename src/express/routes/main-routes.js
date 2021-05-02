61098614331bcbd20fde62ea1afb9a1d5d8732ae
titov-andrei <titov_andrei@mail.ru>
2021-05-02 11:33:16 +0300

    Update main-routes.js

--- src/express/routes/main-routes.js
+++ src/express/routes/main-routes.js
@@ -4,9 +4,21 @@ const { Router } = require(`express`);
 const mainRoutes = new Router();
 const api = require(`../api`).getAPI();
 
+const ARTICLES_PER_PAGE = 8;
+
 mainRoutes.get(`/`, async (req, res) => {
-  const articles = await api.getArticles();
-  res.render(`main`, { articles });
+  let { page = 1 } = req.query;
+  page = +page;
+  const limit = ARTICLES_PER_PAGE;
+  const offset = (page - 1) * ARTICLES_PER_PAGE;
+
+  const [{ count, articles }, categories] = await Promise.all([
+    api.getArticles({ limit, offset }),
+    api.getCategories(true),
+  ]);
+
+  const totalPages = Math.ceil(count / ARTICLES_PER_PAGE);
+  res.render(`main`, { articles, page, totalPages, categories });
 });
 mainRoutes.get(`/register`, (req, res) => res.render(`sign-up`));
 mainRoutes.get(`/login`, (req, res) => res.render(`login`));
@@ -16,11 +28,11 @@ mainRoutes.get(`/search`, async (req, res) => {
     const results = await api.search(search);
 
     res.render(`search`, {
-      results
+      results,
     });
   } catch (error) {
     res.render(`search`, {
-      results: []
+      results: [],
     });
   }
 });
