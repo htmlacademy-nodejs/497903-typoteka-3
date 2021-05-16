7ce9315822f31d8ee334bdd7edfa7daf596d1782
titov-andrei <titov_andrei@mail.ru>
2021-05-16 16:00:48 +0300

    Update main-routes.js

--- src/express/routes/main-routes.js
+++ src/express/routes/main-routes.js
@@ -7,9 +7,14 @@ const api = require(`../api`).getAPI();
 const ARTICLES_PER_PAGE = 8;
 
 mainRoutes.get(`/`, async (req, res) => {
+  // получаем номер страницы
   let { page = 1 } = req.query;
   page = +page;
+
+  // количество запрашиваемых публикаций равно количеству публикаций на странице
   const limit = ARTICLES_PER_PAGE;
+
+  // количество публикаций, которое нам нужно пропустить - это количество публикаций на предыдущих страницах
   const offset = (page - 1) * ARTICLES_PER_PAGE;
 
   const [{ count, articles }, categories] = await Promise.all([
@@ -17,7 +22,10 @@ mainRoutes.get(`/`, async (req, res) => {
     api.getCategories(true),
   ]);
 
+  // количество страниц — это общее количество публикаций, поделённое на количество публикаций на странице (с округлением вверх)
   const totalPages = Math.ceil(count / ARTICLES_PER_PAGE);
+
+  // передадим все эти данные в шаблон
   res.render(`main`, { articles, page, totalPages, categories });
 });
 mainRoutes.get(`/register`, (req, res) => res.render(`sign-up`));
